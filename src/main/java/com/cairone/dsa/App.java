c862e2a7400fdb58ddf582bc36532b32b7c656f5
Diego Cairone <diegocairone@gmail.com>
2025-09-22 12:28:06 -0300

    Initial commit

---
+++ src/main/java/com/cairone/dsa/App.java
@@ -0,0 +1,17 @@
+package com.cairone.dsa;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Data structures and algorithms practice!
+ *
+ */
+public class App {
+
+    private static final Logger log = LoggerFactory.getLogger(App.class);
+
+    public static void main(String[] args) {
+        log.info("data-structure-algorithms-practice-1");
+    }
+}
---
+++ src/test/java/com/cairone/dsa/AppTest.java
@@ -0,0 +1,15 @@
+package com.cairone.dsa;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    @Test
+    void testApp() {
+        Assertions.assertThat(1).isEqualTo(1);
+    }
+}
